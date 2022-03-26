7a69f8d83a10f3f8b834eb422d95ebc117bf8f0c
ahens000 <102434070+ahens000@users.noreply.github.com>
2022-03-27 01:29:05 +0530

    new

---
+++ fifth.py
@@ -0,0 +1,4 @@
+#Label the program written in problem 4 with comments.
+# 1
+
+# I was having a similar issue of Error 305 . Check your playsound version. There is an issue with version 1.3. Downgrading to version 1.2.2 worked for me.
